1af50121e72309fb51e6809a8099db4de802ab3c
RockyMadio <rockymadio@gmail.com>
2020-09-18 18:46:42 +0200

    Changes

--- PhonePanel.gd
+++ PhonePanel.gd
@@ -2,8 +2,8 @@ extends Control
 
 
 func _ready():
-	$VBoxContainer/HBoxContainer/ButtonNo.connect("buttonPressed", self, "OnButtonPressed")
-	$VBoxContainer/HBoxContainer/ButtonYes.connect("buttonPressed", self, "OnButtonPressed")
+	$VBoxContainer/Buttons/ButtonNo.connect("buttonPressed", self, "OnButtonPressed")
+	$VBoxContainer/Buttons/ButtonYes.connect("buttonPressed", self, "OnButtonPressed")
 
 func OnButtonPressed(type):
 	print(type)
