11b53cfcaab200ee8181089d01b4ea9c6ad647c5
RockyMadio <rockymadio@gmail.com>
2020-09-18 19:19:42 +0200

    changed AVATAR code , it is better, pls dont change

--- scripts/AvatarBehaviour.gd
+++ scripts/AvatarBehaviour.gd
@@ -1,17 +1,28 @@
 extends Node
 
-onready var fleshTexture = $SP_Flesh
-onready var torsoTexture = $SP_Torso
-onready var armsTexture = $SP_Arms
-onready var eyesTexture = $SP_Eyes
-onready var mouthTexture = $SP_Mouth
-onready var noseTexture = $SP_Nose
-onready var hairTexture = $SP_Hair
+const lib_path = "res://assets/body_parts/"
+const lib_items ={}
+const part_order = ["SP_Body",
+					"SP_Torso",
+					"SP_Arms",
+					"SP_Eyes",
+					"SP_Mouth",
+					"SP_Nose",
+					"SP_Hair"
+]
+func GET_FILE(FILE):
+	var path2file = lib_path + FILE
+	return load(path2file)
 
-# Called when the node enters the scene tree for the first time.
+func SET_PART(OBJ, BODY_PART):
+	var f = GET_FILE(BODY_PART)
+	if f:
+		OBJ.set_texture(f)
+
+func SET_APPEARANCE(DATA):
+	#DATA = ["WATER","WATER","WATER","FIRE","WATER","AIR","FIRE]
+	for index in range(part_order.size()):
+		SET_PART( get_node(part_order[index]), DATA[index])
+
 func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	pass
