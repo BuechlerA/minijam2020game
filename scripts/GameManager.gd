dee75079f501f51691e0ac41eeb31b43c65f2b44
Alexander Büchler <alexander.buechler25@gmail.com>
2020-09-18 18:00:44 +0200

    Update GameManager.gd

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,16 +1,12 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+	
+func _create_planet():
+	pass
+	
+func _create_alien():
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
